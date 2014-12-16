c5887dd89a23d3268f364a74e9044205f8cf43d6
Matt Keef <mkeef1@gmail.com>
2014-12-16 10:36:56 -0600

    acceptance testing

---
+++ test/acceptance/users.js
@@ -0,0 +1,93 @@
+/* jshint expr:true */
+
+'use strict';
+
+var expect     = require('chai').expect,
+    cp         = require('child_process'),
+    h          = require('../helpers/helpers'),
+    server     = require('../../server/index'),
+    Lab        = require('lab'),
+    lab        = exports.lab = Lab.script(),
+    describe   = lab.describe,
+    it         = lab.it,
+    beforeEach = lab.beforeEach,
+    db         = h.getdb();
+
+
+
+describe('Users', function(){
+  var cookie;
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      var options = {
+        method: 'post',
+        url: '/login',
+        payload: {
+          username: 'bob',
+          password: '1234'
+        }
+      };
+
+      server.inject(options, function(response){
+        cookie = response.headers['set-cookie'][0].match(/hapi-cookie=[^;]+/)[0];
+        console.log(cookie);
+        done();
+      });
+    });
+  });
+
+  describe('post /register', function(){
+    it('should register a new user', function(done){
+      var options = {
+        method: 'post',
+        url: '/register',
+        payload: {
+          username: 'sam',
+          password: '456',
+          avatar: 'http://images.apple.com/global/elements/flags/16x16/usa_2x.png'
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  describe('post /login', function(){
+    it('should login a user', function(done){
+      var options = {
+        method: 'post',
+        url: '/login',
+        payload: {
+          username: 'bob',
+          password: '1234'
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        expect(response.result.username).to.equal('bob');
+        done();
+      });
+    });
+  });
+
+  describe('delete /logout', function(){
+    it('should logout a user', function(done){
+      var options = {
+        method: 'delete',
+        url: '/logout',
+        headers:{
+          cookie:cookie
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+});
